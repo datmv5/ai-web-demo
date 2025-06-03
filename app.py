2245d9d7d0996e7df603289a08e8969b71e77110
datmv5 <datmv05.global@gmail.com>
2025-06-03 17:03:11 +0700

    5

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def index():
     return render_template("index.html", caption=caption)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
